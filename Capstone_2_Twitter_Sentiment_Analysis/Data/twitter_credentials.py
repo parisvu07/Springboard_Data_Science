6de7bf6337aff064cae310d0054cd91e9c50eb53
paris vu <parisvu09@gmail.com>
2022-12-18 23:25:33 -0800

    worked on notebook 3

--- Capstone_2_Twitter_Sentiment_Analysis/Data/twitter_credentials.py
+++
@@ -1,5 +0,0 @@
-# Variables that contain the user credentials to access Twitter API
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
-CONSUMER_KEY = "VwxEjXvHYKKnNHyUAoAUPUGcn"
-CONSUMER_SECRET = "FlmnwlvcGb3xcYDIolShVCGqAeP08Mwvrz1yfWc9tNiyzkW7ID"
